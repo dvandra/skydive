c6566e1ca7c7673e37ff2086eded33a2354eb523
SH118415 <suhu118415@gmail.com>
2018-12-04 22:51:15 +0530

    adding lock for ovs sflow-counter samples

--- sflow/agent.go
+++ sflow/agent.go
@@ -116,8 +116,8 @@ func (sfa *Agent) feedFlowTable() {
 			}
 			logging.GetLogger().Infof("counters= %v", counters)
 			sfa.Graph.Lock()
-			sfa.Graph.Unlock()
 			sfa.Graph.AddMetadata(sfa.Node, "Sflow-Counters", counters)
+			sfa.Graph.Unlock()
 		}
 
 	}
@@ -153,6 +153,7 @@ func (sfa *Agent) Start() {
 
 // Stop the SFlow probe agent
 func (sfa *Agent) Stop() {
+
 	sfa.Lock()
 	defer sfa.Unlock()
 
