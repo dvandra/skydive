e23894afe2e94ea2fc3ad26f46bebc9ee40921a3
Darshan Vandra <dvandra.ai@gmail.com>
2019-01-21 16:11:42 +0530

    Gremlin query for sflow metric : 'G.V().Metrics(sflow)'

--- gremlin/traversal/metrics.go
+++ gremlin/traversal/metrics.go
@@ -79,11 +79,15 @@ func (e *MetricsTraversalExtension) ParseStep(t traversal.Token, p traversal.Gre
 			return nil, errors.New("Metrics parameter have to be a string")
 		}
 		switch k {
-		case "LastUpdateMetric", "SFlow.LastUpdateMetric":
+		case "LastUpdateMetric", "SFlow.LastUpdateMetric", "sflow":
 		default:
 			return nil, fmt.Errorf("Metric field unknown : %v", p.Params)
 		}
-		key = k
+		if k == "sflow" {
+			key = "SFlow.LastUpdateMetric"
+		} else {
+			key = k
+		}
 	default:
 		return nil, fmt.Errorf("Metrics accepts one parameter : %v", p.Params)
 	}
